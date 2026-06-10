3f6326b139f583417ba5596c7d411ed92d36c0cd
Wanli Cheng <wanlicheng@Wanlis-MacBook-Air.local>
2026-06-11 11:20:03 +1200

    Deploy updated site

---
+++ assets/CelsPage-BRc6xpIj.js
@@ -0,0 +1,1 @@
+import{O as e}from"./functionsRequest-BAzk9T7C.js";import{v as t}from"./index-BMn8NSuK.js";import{t as n}from"./SubjectPageLayout-DXlsDhiK.js";var r=e();function i(){let e=t(`cels191`);return e?(0,r.jsx)(n,{subject:e}):(0,r.jsx)(`div`,{className:`p-10`,children:`Subject not found.`})}export{i as default};
