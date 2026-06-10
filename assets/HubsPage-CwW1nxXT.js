496de87b2c98ff94ef40bbe3c67b777e01ac95c3
Wanli Cheng <wanlicheng@Wanlis-MacBook-Air.local>
2026-06-10 23:49:24 +1200

    Deploy updated site

---
+++ assets/HubsPage-CwW1nxXT.js
@@ -0,0 +1,1 @@
+import{O as e}from"./functionsRequest-BAzk9T7C.js";import{f as t}from"./index-ChqsUhKY.js";import{t as n}from"./SubjectPageLayout-C4X-umzq.js";var r=e();function i(){let e=t(`hubs191`);return e?(0,r.jsx)(n,{subject:e}):(0,r.jsx)(`div`,{className:`p-10`,children:`Subject not found.`})}export{i as default};
