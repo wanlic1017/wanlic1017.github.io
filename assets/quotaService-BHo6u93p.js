c9b86c5f7b70691326fe2ba0c6dd5c42568073a3
Wanli Cheng <wanlicheng@Wanlis-MacBook-Air.local>
2026-06-12 15:08:37 +1200

    Deploy updated site

---
+++ assets/quotaService-BHo6u93p.js
@@ -0,0 +1,1 @@
+import{t as e}from"./functionsRequest-B-JQy-KN.js";var t=async t=>e(`/getQuestionDetails`,{method:`POST`,body:{questionIds:t}});export{t};
--- assets/quotaService-Cyv5fqyn.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./functionsRequest-YktzUHSN.js";var t=async t=>e(`/getQuestionDetails`,{method:`POST`,body:{questionIds:t}});export{t};
